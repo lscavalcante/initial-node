79b2bfb2d15c997b567e04a28885a3530eb8bb01
lscavalcante <18038257>
2020-03-30 18:29:58 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+app.set('port',port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+const route = router.get('/', (req, res) => {
+    res.status(200).send({
+        title: 'node store api',
+        version: '0.0.1'
+    });
+});
+
+app.use('/', route);
+
+server.listen(port);
+
+console.log('api rodando na porta ' + port);
