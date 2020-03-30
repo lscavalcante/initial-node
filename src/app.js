63c368436df775b6bcc7dca3e167df12b720a1bb
lscavalcante <18038257>
2020-03-30 19:24:55 -0300

    adicionando o bodyparser que seria o middleware

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
 const route = router.get('/', (req,res,next) => {
     res.status(200).send({
         title: 'Node Store Api',
-        version: "0.0.1"
+        version: "0.0.12"
     });
 });
 
+const create = router.post('/', (req,res,next) => {
+    res.status(201).send(req.body);
+});
+
 app.use('/', route);
+app.use('/products', create);
+
 
 module.exports = app;
