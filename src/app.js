5f1caf2dfcd3d91500ec8c85c0c9007dc9c6217f
lscavalcante <18038257>
2020-03-30 19:32:38 -0300

    criando o crud

--- src/app.js
+++ src/app.js
@@ -19,8 +19,24 @@ const create = router.post('/', (req,res,next) => {
     res.status(201).send(req.body);
 });
 
+const put = router.put('/:id', (req,res,next) => {
+    const id = req.params.id;
+    res.status(201).send({
+        id: id,
+        item:req.body
+    });
+});
+
+const del = router.delete('/', (req,res,next) => {
+    res.status(200).send(req.body);
+});
+
+
+
 app.use('/', route);
 app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 
 module.exports = app;
