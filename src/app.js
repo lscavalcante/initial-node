bf82d3bd3d0bcaa02a2d4c06c7bf34e7ea622ca1
lscavalcante <18038257>
2020-04-02 22:01:54 -0300

    adicionando o mongose

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+// Connecta ao banco
+mongoose.connect("mongodb+srv://admin:admin@node-baita-jtk7c.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
 //carrega as rotas
 const indexRoute = require('./routes/index-route');
